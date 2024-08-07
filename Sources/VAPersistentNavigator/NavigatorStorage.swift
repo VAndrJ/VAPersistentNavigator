9523d8b499e07173743a328d3c3f4ca380161996
Volodymyr Andriienko <vandrjios@gmail.com>
2024-08-07 10:53:55 +0300

    [documentation] NavigatorStorage.

--- Sources/VAPersistentNavigator/NavigatorStorage.swift
+++ Sources/VAPersistentNavigator/NavigatorStorage.swift
@@ -7,10 +7,17 @@
 
 import Foundation
 
+/// A protocol defining the storage mechanism for a navigator.
 public protocol NavigatorStorage {
     associatedtype Destination: Codable & Hashable
     associatedtype TabItemTag: Codable & Hashable
 
+    /// Stores the given navigator.
+    ///
+    /// - Parameter navigator: The `Navigator` to be stored.
     func store(navigator: Navigator<Destination, TabItemTag>)
+    /// Retrieves the stored navigator.
+    ///
+    /// - Returns: The stored `Navigator`, or `nil` if no `Navigator` is stored.
     func getNavigator() -> Navigator<Destination, TabItemTag>?
 }
