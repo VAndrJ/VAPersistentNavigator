17c422aaeb472b748d870723635fb29421a4b63f
Volodymyr Andriienko <vandrjios@gmail.com>
2024-08-07 10:53:47 +0300

    [documentation] NavigatorPresentation.

--- Sources/VAPersistentNavigator/NavigatorPresentation.swift
+++ Sources/VAPersistentNavigator/NavigatorPresentation.swift
@@ -7,8 +7,10 @@
 
 import Foundation
 
+/// Represents the presentation style of the navigator.
 public enum NavigatorPresentation: Codable {
     case sheet
+    /// - Note: Available only on iOS 16.0, tvOS 16.0, and watchOS 9.0 and later.
     @available(iOS 16.0, tvOS 16.0, watchOS 9.0, *)
     case fullScreenCover
 }
