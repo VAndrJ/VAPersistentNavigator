942bed7359eef79e1d240de07557db7b172c57ed
Volodymyr Andriienko <vandrjios@gmail.com>
2025-04-15 14:22:56 +0300

    [example] Open URL.

---
+++ Example/Example/Support/URL+Support.swift
@@ -0,0 +1,12 @@
+//
+//  URL+Support.swift
+//  Example
+//
+//  Created by VAndrJ on 4/15/25.
+//
+
+import Foundation
+
+extension URL {
+    static let apple = URL(string: "https://www.apple.com")!
+}
