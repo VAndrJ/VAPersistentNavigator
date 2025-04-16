9292c70f6db07bb524696f4f1f3710a6750291a2
Volodymyr Andriienko <vandrjios@gmail.com>
2025-04-16 11:25:57 +0300

    [example] Imports for proper syntax highlighting.

--- Example/Example/ExampleFullScreen.swift
+++ Example/Example/ExampleFullScreen.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import VAPersistentNavigator
 
 struct ExampleFullScreen: View {
     let number: Int
--- Example/Example/ExamplePushScreen.swift
+++ Example/Example/ExamplePushScreen.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import VAPersistentNavigator
 
 struct ExamplePushScreen: View {
     let number: Int
