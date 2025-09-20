6f6090a3162a656c3da912183d123c4df0ab0687
Volodymyr Andriienko <vandrjios@gmail.com>
2025-09-20 11:46:09 +0300

    [core] Swift 6.2 updates

--- Package.swift
+++ Package.swift
@@ -1,24 +1,36 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 
 import PackageDescription
 
+let settings: [SwiftSetting] = [
+    .defaultIsolation(MainActor.self),
+    .strictMemorySafety(),
+]
+
 let package = Package(
     name: "VAPersistentNavigator",
-    platforms: [.macOS(.v13), .iOS(.v16), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v13)],
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v16),
+        .tvOS(.v16),
+        .watchOS(.v9),
+        .macCatalyst(.v13),
+    ],
     products: [
         .library(
             name: "VAPersistentNavigator",
             targets: ["VAPersistentNavigator"]
-        ),
+        )
     ],
     targets: [
         .target(
-            name: "VAPersistentNavigator"
+            name: "VAPersistentNavigator",
+            swiftSettings: settings
         ),
         .testTarget(
             name: "VAPersistentNavigatorTests",
-            dependencies: ["VAPersistentNavigator"]
+            dependencies: ["VAPersistentNavigator"],
+            swiftSettings: settings
         ),
-    ],
-    swiftLanguageModes: [.version("6")]
+    ]
 )
